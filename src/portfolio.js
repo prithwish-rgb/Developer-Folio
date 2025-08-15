1e7723e0e98c12a794c3db7be71d938fdb82240f
prithwish-rgb <prithwishkarmakar22@gmail.com>
2025-08-15 12:50:06 +0530

    Initial commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,7 +20,7 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Prithwish ",
+  username: "Prithwish K",
   title: "Hi all, I'm Prithwish",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
